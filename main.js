b36c97d87420d7e565f93bf5e9612f96a94c2c6d
Monicavila <di.monicavila@gmail.com>
2020-10-14 18:06:41 -0500

    check misspellings

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ function select() {
     const selectedFunction = libreryFunctions.find((eachFunction) => 
     eachFunction.name == selectAFunctionValue);
 
+    // display form inputs
+    var y = document.getElementById('data1');
+    var x = document.getElementById('data2');
+    if (y.style.display === 'none') {
+        y.style.display = 'block';
+    }
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    }
+    if (selectAFunctionValue === 'noSelection') {
+        y.style.display = 'none';
+        x.style.display = 'none';
+    } 
+
     document.getElementById('explanation').innerHTML= selectedFunction.text;
     // Link placeholder text displayed with selectedFunction
     document.getElementById('data1').placeholder = selectedFunction.placeholder1;
@@ -15,12 +29,20 @@ function select() {
     });*/
 }
 
+function display() {
+    
+}
+
 const libreryFunctions = [
+    {
+        name: 'noSelection',
+        text: `<p><strong>Welcome</strong> and practice a few! 8 functions coded in javaScript.</p>`
+    },
     {
         name: 'ForEach',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>for Each</strong>, wich <a>iterates over the array items, in ascending order.</a>,
+            <p>Implement method <strong>forEach</strong>, which <a>iterates over the array items, in ascending order</a>.
             The provided function may perform any kind of <a>operation</a> on the elements of the given array. 
             <em>And returns the index of each object too</em>.</p>
             <br>
@@ -55,16 +77,16 @@ const libreryFunctions = [
         name: 'Filter',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>filter</strong>, wich <a>creat a new array</a>,
+            <p>Implement method <strong>filter</strong>, which <a>creats a new array</a>,
             with the elements <em>that have pass the requirement (predicate) check</em>.</p>
             <br>
             <p>&nbsp;For example</p> 
             <br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want only <strong>the even</strong> 
-            <a>numbres</a> in the array</p>
+            <a>numbers</a> in the array</p>
             <br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = [[1, 2, 7, 10];</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Predicate</strong> : evens numbres</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Predicate</strong> : evens numbers</p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like <strong>array.filter(% 2 === 0);</strong></p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer [2, 10]</p>
             <br>
@@ -90,7 +112,7 @@ const libreryFunctions = [
         name: 'Map',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>map</strong>, wich <a>creats a new array by using callback</a>,
+            <p>Implement method <strong>map</strong>, which <a>creats a new array by using callback</a>,
             on each one of the elements, on each iteration <em>we need arguments like the array itself, 
             current element and the index</em>.</p>
             <br>
@@ -126,12 +148,12 @@ const libreryFunctions = [
         name: 'Find',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>find</strong>, wich <a>returns the first element</a>,
-            in the array <em>that satisfies the requirement (predicate) function</em>.</p>
+            <p>Implement method <strong>find</strong>, which <a>returns the first element</a>,
+            in the array<em> that satisfies the requirement (predicate) function</em>.</p>
             <br>
             <p>&nbsp;For example</p> 
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know wich is <a>the first number </a><strong>less than 70</strong>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know which is <a>the first number</a><strong> less than 70</strong>
             in the array</p>
             <br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = [45, 82, 34, 15, 77];</p>
@@ -161,22 +183,22 @@ const libreryFunctions = [
         name: 'FindIndex',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>findIndex</strong>, wich <a>returns the index of the first</a>,
-            item to search in the array or predicate. <em>The default index optonal argument to start is 0</em>.</p>
+            <p>Implement method <strong>findIndex</strong>, which <a>returns the index of the first</a>,
+            item to search in the array or predicate. <em>The default index optional argument to start is 0</em>.</p>
             <br>
             <p>&nbsp;For example</p> 
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know the positon, <a>index value</a>, of an element <strong>'panda'</strong>  
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know the position, <a>index value</a>, of an element <strong>'panda'</strong>  
             in the array</p>
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = ['pinguin', 'jifar', 'shark', 'panda'];</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = ['penguin', 'giraffe', 'shark', 'panda'];</p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Predicate</strong> : 'panda'</p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like:
             <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array.findIndex(element => element === 'panda');</strong></p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer 3</p>
             <br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>itemToSearch</strong> : 'panda'</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;An alternative to implement with itmeToSearch is like:
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;An alternative to implement with itemToSearch is like:
             <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array.indexOf(itemToSearch)</strong></p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer 3</p>
@@ -199,52 +221,53 @@ const libreryFunctions = [
         name: 'Contains',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>pluck</strong>, wich <a>takes an array of objects and a property name</a>,
-            and <em>returns an array containing the named property of each object</em>.</p>
+            <p>Implement method <strong>contains</strong>, which <a>returns true</a> if the value is present in the list (an array could be).
+            <em>iterates over the array items, in ascending order</em>. Maybe you need to start in certain position of the array
+            then apply fromIndex. Other alias: <strong>include, includes.</strong></p>
             <br>
             <p>&nbsp;For example</p> 
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know the <strong>'lenght'</strong> <a>of each one</a> 
-            of the objects in the array</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know if <strong>'a' </strong><a>is present</a> 
+            in the objects of the array</p>
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = [dog, cats, elephant, carrot];</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Property name</strong> : 'lenght'</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like _.pluck(array, 'lenght');</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer [3, 4, 8, 6]</p>
-            <br>
-            
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = ['a', 'b', 'c', 'd'];</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Value</strong> : 'a'</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like:
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array.contains(element => element === 'a');</strong></p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer <em>true<em></p>
+            <br>           
         </div>
         `,
         placeholder1: `Enter: [array]`,
-        placeholder2: `Enter: 'Property name'`,
+        placeholder2: `Enter: the value you want to know if is present`,
 
-        code:  function myPluck(objs, name)  {
-                var array = [];
-                var i;
-                for(i = 0; i < objs.length; i++) {
-                    if(objs[i].hasOwnProperty(name)) {
-                        array.push(objs[i][name]);
+        code:  Array.prototype.myContains = function (callback) {
+                    for(let i = 0; i < this.length; i++) {
+                    if(callback(this[i])) {
+                            return true;
+                        }
                     }
-                }
-                return array;
-        }
-        //console.log(myPluck([objs], 'name')),
+                    return false;
+                },
+                    
+                //console.log(array.myContains(element => element === 83));
     },
     {
         name: 'Pluck',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>pluck</strong>, wich <a>takes an array of objects and a property name</a>,
+            <p>Implement method <strong>pluck</strong>, which <a>takes an array of objects and a property name</a>,
             and <em>returns an array containing the named property of each object</em>.</p>
             <br>
             <p>&nbsp;For example</p> 
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know the <strong>'lenght'</strong> <a>of each one</a> 
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know the <strong>'length'</strong> <a>of each one</a> 
             of the objects in the array</p>
             <br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = [dog, cats, elephant, carrot];</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Property name</strong> : 'lenght'</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like _.pluck(array, 'lenght');</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Property name</strong> : 'length'</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like <strong>pluck(array, 'length');</strong></p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer [3, 4, 8, 6]</p>
             <br>
             
@@ -269,24 +292,24 @@ const libreryFunctions = [
         name: 'Without',
         text: 
         `<div style="text-align: justify">
-            <p>Implement method <strong>pluck</strong>, wich <a>takes an array of objects and a property name</a>,
-            and <em>returns an array containing the named property of each object</em>.</p>
+            <p>Implement method <strong>without</strong>, which <a>returns an array copy</a>,
+            with all <em>items values</em> that are not removed because them not match the stablished property value.</p>
             <br>
             <p>&nbsp;For example</p> 
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to know the <strong>'lenght'</strong> <a>of each one</a> 
-            of the objects in the array</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;If you want to remove <strong>'c' & 'b'</strong> <a>from the array</a> 
+            and get all the other letters</p>
             <br>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = [dog, cats, elephant, carrot];</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Property name</strong> : 'lenght'</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like _.pluck(array, 'lenght');</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer [3, 4, 8, 6]</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>array</strong> = ['a', 'b', 'c', 'd', 'e'];</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Values to remove</strong> : 'c' & 'b'</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You implement this method like <strong>array.without('c', 'b');</strong></p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The answer ['a', 'd', 'e']</p>
             <br>
             
         </div>
         `,
         placeholder1: `Enter: [array]`,
-        placeholder2: `Enter: '(items, you want to removecs)`,
+        placeholder2: `Enter: items values, you want to remove`,
 
         code:   Array.prototype.myWhithout = function(...items) {
                     const copyArray = Array.from(array);
